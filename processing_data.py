76842450a032eebda8eb5400b88169b898cee4ef
Samar-mami <samar.mami18@gmail.com>
2023-11-22 11:35:00 +0100

    merge dataframens and to json functions implementations

--- processing_data.py
+++ processing_data.py
@@ -1,13 +1,31 @@
 import pandas as pd
 
 
-def find_drug_name(df_drugs, drug_name_column, df_search, title_column):
+def find_drug_name(df_reference, referance_name_column, df_search_in, title_search_in_column):
     df_found = pd.DataFrame(columns=['drug', 'referenced_in', 'date_mention'])
-    for drug in df_drugs[f'{drug_name_column}']:
-        for title in df_search[f'{title_column}']:
+    for drug in df_reference[f'{referance_name_column}']:
+        for title in df_search_in[f'{title_search_in_column}']:
             if drug in title:
-                date_index = df_search[df_search[f'{title_column}'] == title].index[0]
+                date_index = df_search_in[df_search_in[f'{title_search_in_column}'] == title].index[0]
                 # Append the data to df_found
-                row = [drug, title, df_search.loc[date_index, 'date']]
+                row = [drug, title, df_search_in.loc[date_index, 'date']]
                 df_found.loc[len(df_found)] = row
     return df_found
+
+
+def merge_dataframes(df1, def2, merge_column):
+    try:
+        print("Merging Dataframes on: "f'{merge_column}', "...")
+        final_df = pd.merge(df1, df1, on=f'{merge_column}')
+        return final_df
+    except Exception as e:
+        print("An error has occurred: "f'{e}')
+
+
+def convert_df_to_json(df):
+    try:
+        print("Converting Dataframe to json...")
+        json = df.to_json()
+        return json
+    except Exception as e:
+        print("An error has occurred: "f'{e}')
